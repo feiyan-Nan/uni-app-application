94cee057cc37bc4df2050c49379841247619256a
nanfeiyan <guigong.li@mail.hypers.com>
2021-08-26 16:03:55 +0800

    init project

---
+++ App.vue
@@ -0,0 +1,22 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/* 引入官方的样式库 */
+	@import url("/common/uni.css");
+	/* 引入自己的样式库 */
+	@import url("/common/common.css");
+	
+	@import url("/common/free.css");
+</style>
